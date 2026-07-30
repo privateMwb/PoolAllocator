8829ea9423479ae5c6d55f9fa165f83ac3d28ee0
Rain Lee <private.m.w.b.21@gmail.com>
2026-07-30 15:04:47 +0800

    release

--- build-tidy/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool
-# Build directory: /data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/build-tidy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests_build")
--- build-tidy/tests_build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/tests
-# Build directory: /data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/build-tidy/tests_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[PoolProTests]=] "/data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/build-tidy/tests")
-set_tests_properties([=[PoolProTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/tests/CMakeLists.txt;32;add_test;/data/data/com.termux/files/home/projects/C++/AllocatorPro/Pool/tests/CMakeLists.txt;0;")
